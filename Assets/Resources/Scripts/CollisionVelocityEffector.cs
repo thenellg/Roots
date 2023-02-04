7ae79ebe9ef9b978be944043a794e414f44c90cb
Griffin Thenell <griffin.thenell@serenityforge.com>
2023-02-04 15:10:52 -0800

    Fixed issues

--- Assets/Resources/Scripts/CollisionVelocityEffector.cs
+++ Assets/Resources/Scripts/CollisionVelocityEffector.cs
@@ -65,9 +65,11 @@ RaycastHit2D[] results = Physics2D.RaycastAll(transform.position+new Vector3(.1f
 bool isOnTop = false;
 foreach(RaycastHit2D hit in results){
 if(hit.rigidbody==collision.rigidbody)isOnTop = true;
+
+if (collision.gameObject.tag == "Player" && ((collision.transform.position.y - gameObject.transform.position.y) > 0))
+    isOnTop = true;
 }
 
-/*
 if(isOnTop){
 if(!passengers.Contains(collision.gameObject))passengers.Add(collision.gameObject);
 if(CollisionType==Type.OnCollision){
@@ -79,7 +81,7 @@ GetComponent<AudioSource>().Play();
 }
 
 }
-*/
+
 
 }
 }
